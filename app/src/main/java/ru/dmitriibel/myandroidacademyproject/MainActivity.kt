8488da45d02c93cf801ce2afe64c29d19b0facbc
Dmitrii Belykh <dmitrybelykh@yandex.ru>
2020-11-29 11:45:33 +0300

    Move to single activity & fragment

--- app/src/main/java/ru/dmitriibel/myandroidacademyproject/MainActivity.kt
+++ app/src/main/java/ru/dmitriibel/myandroidacademyproject/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.dmitriibel.myandroidacademyproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dmitriibel.myandroidacademyproject.moviescreen.view.FragmentMovieInfo
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showMovieInfoFragment()
+    }
+
+    private fun showMovieInfoFragment(param1: String = "", param2: String = "") {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.content_frame, FragmentMovieInfo.newInstance(param1, param2), FragmentMovieInfo::class.simpleName)
+                .addToBackStack(null)
+                .commit()
     }
 }
